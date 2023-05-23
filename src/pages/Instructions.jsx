d547b027636807cb8adca5e79c16887b7796607c
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-05-23 12:32:08 +0530

    added button

--- src/pages/Instructions.jsx
+++ src/pages/Instructions.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import { trusted } from 'mongoose'
 
 const Instructions = () => {
   return (
@@ -70,6 +71,95 @@ const Instructions = () => {
 <p className='py-1 px-6 font-semibold  text-[#565151] text-[15px]'>B: A fraction (e.g. -0.3 or -.3) can be entered as an answer with or without “0” before the decimal point. As many as four decimal points, e.g. 12.5435 or 0.003 or -932.6711 or 12.82 can be entered.</p>
 
 <p className='py-1 px-6 font-semibold  text-[#565151] text-[15px]'>C: To save your answer, you MUST click on the Next.</p>
+
+<p className='py-1 px-4  font-semibold text-[15px]'>6: To mark a question for review, click on the Star button in top right corner of the screen. If an answer is selected (for MCQ/MCAQ) or entered (for numerical answer type) for a question that is Marked for Review , that answer will be considered in the evaluation unless the status is modified by the candidate.</p>
+
+<p className='py-1 px-4  font-semibold text-[15px]'>7: To change your answer to a question that has already been answered, first select that question for answering and then follow the procedure for answering that type of question.</p>
+
+<p className='py-1 px-4  font-semibold text-[15px]'>8: Note that ONLY Questions for which answers are saved or marked for review after answering will be considered for evaluation.</p>
+
+<p  className='py-1 px-4  font-semibold text-[15px]'>9: Sections in this question paper are displayed on the top bar of the screen. Questions in a Section can be viewed by clicking on the button in lower right corner. The Section you are currently viewing</p>
+
+<p className='py-1 px-4  font-semibold text-[15px]'> 10: After clicking the Next button for the last question in a Section, you will automatically be taken to the first question of the next section in sequence.</p>
+
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-[80%] text-sm text-left text-gray-500 dark:text-gray-400 mx-auto mt-4">
+        <thead class="text-xs text-white uppercase bg-blue-500  dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                SI No.1
+                </th>
+          
+                <th scope="col" class="px-6 py-3">
+                Section Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                No. of Question
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Maximum Marks
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Negative Marks
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Positive Marks
+                </th>
+            </tr>
+        </thead>
+        <tbody >
+            <tr class="bg-white  dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+             1
+                </th>
+                <td class="px-6 py-4">
+                Test
+                </td>
+                <td class="px-6 py-4">
+                50
+                </td>
+                <td class="px-6 py-4">
+                100
+                </td>
+                <td class="px-6 py-4">
+                0.66
+                </td>
+
+                <td class="px-6 py-4">
+                2
+                </td>
+          
+            </tr>
+    
+     
+        </tbody>
+    </table>
+</div>
+
+
+<p className='py-1 px-4  font-semibold text-[15px]'>1. A total of 60 minutes is allotted for the examination.</p>
+
+<p className='py-1 px-4  font-semibold text-[15px]'>2.The server will set your clock for you. In the top right corner of your screen, a countdown timer will display the remaining time for you to complete the exam. Once the timer reaches zero, the examination will end automatically. The paper need not be submitted when your timer reaches zero.</p>
+
+<p className='py-1 px-4  font-semibold text-[15px]'>3. There will, however, be sectional timing for this exam. You will have to complete each section within the specified time limit. Before moving on to the next section, you must complete the current one within the time limits.</p>
+
+<div className="flex items-center border mt-10">
+     <input className='scale-[1.2] ml-6' type="checkbox" />
+     <label className='py-1 px-4  font-normal leading-1 text-[15px]' >
+I have read all the instructions carefully and have understood them. I agree not to chat or use unfair means in examinations. I understood that using unfair means of any sort for my own or someone else’s advantage will lead to my immediate disqualification. The decision of Prepp.in will be final in these matters & cannot be appealed.</label>
+</div>
+
+<div className='justify-center  flex gap-5 py-2 px-4 ml-10 border'>
+     <button  className='border  w-36 px-2 py-2 rounded bg-amber-400' >Back to test</button>
+     <button  className='border w-36 px-6 py-2 rounded bg-amber-400'>Next</button>
+</div>
+
+
+
+
+
+
 
       </div>
 
