4ea69e6c8d90463a3a7660a528caae1da8ae8978
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-05-25 11:47:38 +0530

    added button color

--- src/components/QSidebar.jsx
+++ src/components/QSidebar.jsx
@@ -46,7 +46,7 @@ const QSidebar = () => {
       <div className='grid grid-cols-4 gap-3 p-2 overflow-y-scroll h-[200px]'>
       {questions.map((el)=>{
  return   <div key={el.id} >
-     <button className='border bg-slate-600 px-2 py-2 w-16 h-9 rounded-full text-[white]'>{el.id}</button>
+     <button className='border bg-gray-500 px-2 py-2 w-16 h-9 rounded text-[white]'>{el.id}</button>
   </div>
 })}
       </div>
