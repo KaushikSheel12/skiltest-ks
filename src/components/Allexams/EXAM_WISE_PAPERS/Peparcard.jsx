78fc4c64aeea827650c6d6acfc402618034f4358
shivamonside <shivam.dubey@skilzen.com>
2023-06-09 17:29:31 +0530

    added pepar's page

---
+++ src/components/Allexams/EXAM_WISE_PAPERS/Peparcard.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { BsBookHalf } from 'react-icons/Bs'
+
+const Peparcard = () => {
+  return (
+  <>
+        <div className='md:w-56 md:h-[270px] group cursor-pointer border border-gray-200 md:px-1 px-0 md:py-2  py-1 rounded-md md:mt-4 mt-2 justify-center text-center hover:shadow-xl transition-all md:block flex content-center h-16'>
+
+<div className='bg-gray-200   mx-auto md:mt-3 md:w-9 md:h-9 h-12 w-12 flex justify-center items-center rounded-full'><BsBookHalf   size={20}/>
+</div>
+
+<div className='mt-1'>
+<h2 className='font-semibold py-1 text-gray-600 md:text-[15px] text-[13px]'>UPPCS 2020 CSAT Paper</h2>
+
+<div className=" md:hidden flex px-2 py-1 md:mt-2 text-gray-600 gap-2 text-[10px] -mt-2">
+
+<p>3 Attempts</p>
+</div>
+</div>
+
+
+
+<div className="hidden md:flex px-2 py-1 md:mt-2 text-gray-600 justify-between">
+<p><span className='w-1 h-1 bg-gray-600
+rounded-full inline-flex mb-[4px] '></span> Attempts</p>
+<p>3</p>
+</div>
+
+
+<div className="hidden md:flex px-2  py-1 text-gray-600 justify-between">
+<p><span className='w-1 h-1 bg-gray-600
+rounded-full inline-flex mb-[4px] mr-1'></span>Quetions</p>
+<p>50</p>
+</div>
+
+
+<div className="hidden md:flex px-2 text-gray-600  py-1 justify-between">
+<p><span className='w-1 h-1 bg-gray-600
+rounded-full inline-flex mb-[4px] mr-1'></span>Max marks</p>
+<p>100</p>
+</div>
+
+
+<div className='border text-white font-semibold mx-auto border-orange-500 md:w-48 w-16 md:text-[16px] text-[10px] flex justify-center rounded-md items-center md:mt-3 md:h-9 mt-2 h-6 text-center group-hover:bg-orange-500 group-hover:text-white md:bg-white bg-orange-600 md:text-orange-600 '>
+START <span className='hidden ml-2 md:inline-block'> TEST </span>
+</div>
+
+</div>
+  </>
+  )
+}
+
+export default Peparcard
--- src/components/Allexams/EXAM_WISE_PAPERS/Pepars.jsx
+++ src/components/Allexams/EXAM_WISE_PAPERS/Pepars.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { BsBookHalf } from 'react-icons/Bs'
 import { RiGovernmentLine } from 'react-icons/Ri'
+import Peparcard from './Peparcard'
 
 const Pepars = () => {
   return (
    <>
-   <div className='md:w-[90%] md:mx-auto mt-20 w-full h-full px-2 py-2'>
+   <div className='md:w-[90%] md:mx-auto md:mt-20 mt-16 w-full h-full px-2 py-2'>
     <h1 className='md:text-[27px] font-bold'>UP State Exams Practice Set Papers, Model Sample Papers</h1>
     <p className='text-gray-600 md:text-[14px] text-[12px]'> Start practicing with UP State  practice set papers free at College menter's Exam Prep. The UP State exams previous  year solved papers 2020/2019/2018/2017/2016 are available online with the  solutions and answers.</p>
    </div>
        
 
-   <div className='md:w-[100%] md:h-[200px] h-[135px] border md:mt-5 bg-indigo-100'>
+   <div className='md:w-[100%] md:h-[200px] h-[120px] border md:mt-5 bg-indigo-100'>
 
      <div className='md:w-[90%] md:mx-auto  w-full h-full px-2 py-2'>
         <h2 className='font-semibold md:text-[19px]'>EXAM WISE PAPERS</h2>
@@ -27,7 +29,21 @@ const Pepars = () => {
             
           
         </div>
+
+        <h2 className='md:mt-16 mt-6 font-semibold md:py-2 md:text-[25px]'>ALL UPPSC PRACTICE PAPERS</h2>
+        <p className='text-gray-500 md:text-[14px] text-[12px] md:-mt-2 mt-0'>View all year-wise practice papers of UPPSC Exams</p>
+
+
+        <div className=' md:mt-4 mt-2 bg-red-100 md:w-20 w-12 md:h-8 h-6 rounded-md md:top-10 flex items-center justify-center text-[13px] md:text-[18px]'>
+        2022
+        </div>
+
+
+
+       {/* Card */}
+  <Peparcard/>
      </div>
+  
    </div>
    </>
   )
