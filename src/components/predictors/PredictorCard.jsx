17f28eec78e4be3ecc31f553c9f0ac5c34278962
shivamonside <shivam.dubey@skilzen.com>
2023-07-18 15:40:49 +0530

    added predictors

--- src/components/predictors/PredictorCard.jsx
+++ src/components/predictors/PredictorCard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { TiTick } from "react-icons/Ti";
+import ClgBox from "./ClgBox";
 
 const PredictorCard = () => {
   return (
-    <div>PredictorCard</div>
-  )
-}
+    <>
+      <div className="border border-gray-300 bg-white px-2 py-1 mt-4  h-96">
+        <h4 className="md:text-[14px] text-[12px]  text-[#3b5eb0] font-semibold">
+          St Mary's Engineering College, Bhuvanagiri
+        </h4>
+        <p className="text-gray-500 py-1  md:text-[13px] text-[11px] ">Ownership: Private</p>
 
-export default PredictorCard
+        <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-0 py-1 mt-4 ">
+          <h3 className="md:text-sm text-[11px] font-semibold  text-gray-500">
+            Your Chances are{" "}
+            <span className="bg-[#1a9d1a]   text-white px-1  rounded-md">
+              Good
+            </span>{" "}
+            in 6 out of Courses
+          </h3>
+
+          <ClgBox />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PredictorCard;
--- src/components/predictors/Predictors.jsx
+++ src/components/predictors/Predictors.jsx
@@ -1,83 +1,51 @@
 import React from "react";
 import Pheader from "./Pheader";
 import { TiTick } from "react-icons/Ti";
+import PredictorCard from "./PredictorCard";
 
 const Predictors = () => {
   return (
     <>
       <Pheader />
-      <div className="flex h-full bg-slate-50  w-[80%] mx-auto py-2 px-2  gap-4  ">
-        <div className="border border-red-500 h-6 w-1/3  text-center">
+      <div className="md:hidden  border w-[90%] grid mx-auto mt-2 rounded-md  px-2 py-2  h-20 shadow-xl">
+        <p className="font-semibold text-md px-1 text-gray-700">
+          Summary of Your Prediction
+        </p>
+        <div className="flex justify-around border-b-2 py-1 border-gray-400">
+          <div className="flex gap-2 items-center justify-center">
+            <TiTick size={19} color="green" />
+            <p className="text-sm text-gray-600"> Your Chances are Good in :</p>
+          </div>
+
+          <p className="text-[#3b5eb0] font-semibold text-sm">
+            4 Colleges &gt;{" "}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex h-full bg-slate-50 md:mt-0 mt-2  md:w-[80%] w-[90%]  mx-auto py-2 px-2  gap-4  ">
+        <div className="border md:block hidden border-red-500 h-6 w-1/3  text-center">
           FILTER'S
         </div>
-        <div className="border border-red-500 h-full  py-2 px-2  w-[50%]   ">
-          <div className="border flex justify-between items-center   border-green-500  h-10 w-full  bg-[#d5f9d5]  mx-auto rounded-sm  py-0 px-2">
-            <h2 className="text-[15px]">Get Personalised Report in PDF </h2>
-            <div className="flex justify-center border text-sm cursor-pointer  text-white  bg-[#209120] px-2 py-[2px] w-20 h-7  rounded-full  items-center">
+        <div className="border  h-full  py-2 px-2  md:w-[50%] w-[100%]   ">
+          <div className="border flex justify-between items-center   md:border-green-500  h-10 w-full rounded-full    bg-[#d5f9d5]  mx-auto md:rounded-sm  py-0 px-2">
+            <h2 className="md:text-[15px] text-[13px]">Get Personalised Report in PDF </h2>
+            <div className="flex justify-center border text-sm cursor-pointer  text-white  bg-[#209120] px-2 py-[2px] md:w-20 w-16  md:h-7  h-6 rounded-full  items-center">
               <TiTick size={19} color="black" />
               Send
             </div>
           </div>
-          <h3 className="text-[16px] font-semibold  py-1 ">
+          <h3 className="md:text-[16px] text-[12px] font-semibold  py-1 ">
             Showing 2 Colleges in TS EAMCET Counselling
           </h3>
 
-
-<div className="border border-black bg-white px-2 py-1 mt-4  h-96">
-    <h4 className="text-[14px] text-[#3b5eb0] font-semibold">St Mary's Engineering College, Bhuvanagiri</h4>
-    <p className="text-gray-500 py-1  text-[13px] ">Ownership: Private</p>
-
-    <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-2 py-2 mt-4 "></div>
-</div>
-
-
-
-
-<div className="border border-black bg-white px-2 py-1 mt-4  h-96">
-    <h4 className="text-[14px] text-[#3b5eb0] font-semibold">St Mary's Engineering College, Bhuvanagiri</h4>
-    <p className="text-gray-500 py-1  text-[13px] ">Ownership: Private</p>
-
-    <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-2 py-2 mt-4 "></div>
-</div>
-
-
-<div className="border border-black bg-white px-2 py-1 mt-4  h-96">
-    <h4 className="text-[14px] text-[#3b5eb0] font-semibold">St Mary's Engineering College, Bhuvanagiri</h4>
-    <p className="text-gray-500 py-1  text-[13px] ">Ownership: Private</p>
-
-    <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-2 py-2 mt-4 "></div>
-</div>
-
-
-<div className="border border-black bg-white px-2 py-1 mt-4  h-96">
-    <h4 className="text-[14px] text-[#3b5eb0] font-semibold">St Mary's Engineering College, Bhuvanagiri</h4>
-    <p className="text-gray-500 py-1  text-[13px] ">Ownership: Private</p>
-
-    <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-2 py-2 mt-4 "></div>
-</div>
-
-
-<div className="border border-black bg-white px-2 py-1 mt-4  h-96">
-    <h4 className="text-[14px] text-[#3b5eb0] font-semibold">St Mary's Engineering College, Bhuvanagiri</h4>
-    <p className="text-gray-500 py-1  text-[13px] ">Ownership: Private</p>
-
-    <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-2 py-2 mt-4 "></div>
-</div>
-
-
-<div className="border border-black bg-white px-2 py-1 mt-4  h-96">
-    <h4 className="text-[14px] text-[#3b5eb0] font-semibold">St Mary's Engineering College, Bhuvanagiri</h4>
-    <p className="text-gray-500 py-1  text-[13px] ">Ownership: Private</p>
-
-    <div className=" border-t-2 border-b-2  border-gray-200 h-60 px-2 py-2 mt-4 "></div>
-</div>
-
-
-
-
-
-
-
+          <PredictorCard />
+          <PredictorCard />
+          <PredictorCard />
+          <PredictorCard />
+          <PredictorCard />
+          <PredictorCard />
+          <PredictorCard />
         </div>
       </div>
     </>
