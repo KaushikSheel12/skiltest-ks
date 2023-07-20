c56cdc9a3da1cf7666ef08fe6def88e8137ace36
shivamonside <shivam.dubey@skilzen.com>
2023-07-20 11:17:24 +0530

    added

--- src/components/careerhomepage/careerimg/Clg.jsx
+++ src/components/careerhomepage/careerimg/Clg.jsx
@@ -4,9 +4,10 @@ import React from "react";
 const Clg = () => {
   return (
     <>
-      <div className="w-[80%]  h-auto mx-auto py-2 justify-center gap-4  px-2 mt-4 flex">
-        <div className="w-[40%] h-[500px] py-2 px-2 ">
-          <div className=" w-full h-56 relative rounded-r-full  ">
+      <div className="max-w-[980px] border  h-auto mx-auto py-2 justify-between  gap-4  mt-4 flex">
+        <div className="">
+        <div className=" h-[500px] py-2 ">
+          <div className=" md:w-full min-w-[400px]   h-56 relative rounded-r-full  ">
             <Image
               src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2F24e726335bbacb2fd08ed75782b1ecff.cdn.bubble.io%2Ff1643878160011x270108937798531300%2Ffood_two%25201.png?w=512&h=249&auto=compress&dpr=2&fit=max"
               fill
@@ -15,7 +16,7 @@ const Clg = () => {
           </div>
           <div className="w-full flex py-2 mt-16  px-2 h-10 gap-6  items-center  ">
             <div className="bg-green-600 w-20 h-1"></div>
-            <div className="text-green-600  font-semibold  text-[27px]">
+            <div className="text-green-600  font-semibold  text-[27px] whitespace-nowrap">
               Across the U.S.
             </div>
           </div>
@@ -24,6 +25,8 @@ const Clg = () => {
             and fries you crave, choose from over 300,000 local and national
             favorites
           </p>
+        </div>
+
         </div>
 
         <div className="w-[8%] writing-mode-vertical text-center md:block hidden   py-2 text-[40px]  h-[500px] ">
@@ -36,11 +39,12 @@ const Clg = () => {
           <h1 className="text-[red]">C</h1>
           <h1 className="text-[green]">E</h1>
         </div>
+<div className="">
 
-        <div className="w-[40%] h-[500px]  py-2 px-2  ">
+        <div className="w-[ h-[500px]  py-2   ">
           <div className="w-full flex py-2   px-2 h-10 gap-6  justify-end  mt-6  items-center  ">
             <div className="bg-green-600 w-20 h-1"></div>
-            <div className="text-green-600  font-semibold  text-[27px]">
+            <div className="text-green-600  font-semibold  text-[27px] whitespace-nowrap ">
               Across the U.S.
             </div>
           </div>
@@ -50,7 +54,7 @@ const Clg = () => {
             favorites
           </p>
 
-          <div className=" w-full h-56 relative rounded-l-full mt-16  ">
+          <div className=" w-full   h-56 relative rounded-l-full mt-16  ">
             <Image
               src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2F24e726335bbacb2fd08ed75782b1ecff.cdn.bubble.io%2Ff1643878173886x562651702202836560%2Ffood_one%25201.png?w=512&h=256&auto=compress&dpr=2&fit=max"
               fill
@@ -58,6 +62,7 @@ const Clg = () => {
             />
           </div>
         </div>
+</div>
       </div>
     </>
   );
