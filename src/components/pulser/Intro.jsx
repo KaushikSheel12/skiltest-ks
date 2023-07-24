252f8b23e991eaafe1b068478e11af5a27c36d12
shivamonside <shivam.dubey@skilzen.com>
2023-07-24 14:23:26 +0530

    added about-section

---
+++ src/components/pulser/Intro.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+  <>
+          <div className="w-[80%] mx-auto h-fit flex justify-center  text-center    p-6 mt-20">
+          <div className=" w-[50%] ">
+            <div className="w-fit h-fit px-2 py-1 mx-auto  rounded-lg flex items-center text-sm font-semibold bg-[#68dd68] text-[#1e451e] leading-6  ">
+              INTRO
+            </div>
+            <h1 className="text-[35px]  font-bold text-[#2d652d] py-2 leading-[42px]">
+              Let's build your next big idea with Pulsar a Non Code Template
+            </h1>
+            <p className="text-[18px]  font-semibold text-[gray] text-center  py-1">
+              As easy as just double click and type to edit
+            </p>
+          </div>
+
+
+
+
+        </div>
+  </>
+  )
+}
+
+export default Intro
--- src/components/pulser/TopHeroSection.jsx
+++ src/components/pulser/TopHeroSection.jsx
@@ -1,16 +1,63 @@
 import React from "react";
 import ScrollingText from "./ScrollingText";
 import Image from "next/image";
+import Intro from "./Intro";
+import { FaHandshake, FaServicestack } from "react-icons/Fa";
+import AboutBox from "./AboutBox";
+import { RiMoneyDollarBoxLine } from "react-icons/Ri";
+import { BsArrowDown } from "react-icons/Bs";
+
+const aboutBoxData = [
+  {
+    text: "About Us",
+    pharagraf: "Pulsar template boasts a visually striking and modern design",
+    icon: (
+      <FaHandshake
+        size={70}
+        color="teal"
+        className="border rounded-full bg-[#99c299] mx-auto py-1 px-2 "
+      />
+    ),
+  },
+
+  {
+    text: "Our Services",
+    pharagraf: "Fully responsive, adapting to different screen sizes",
+    icon: (
+      <FaServicestack
+        size={70}
+        color="teal"
+        className="border rounded-full bg-[#99c299] mx-auto py-1 px-2 "
+      />
+    ),
+  },
+
+  {
+    text: "Contact Us",
+    pharagraf: "Pulsar offers a wide array of thoughtfully designed sections",
+    icon: (
+      <RiMoneyDollarBoxLine
+        size={70}
+        color="teal"
+        className="border rounded-full bg-[#99c299] mx-auto py-1 px-2 "
+      />
+    ),
+  },
+];
 
 const TopHeroSection = () => {
   return (
     <>
       <div className="h-screen">
-        <div className="   bg-[#edfbed] h-[550px] border border-black relative">
+        <div className="  py-1  bg-[#edfbed] h-[550px] relative">
           {/* Your page content here */}
 
           <div className="  w-[90%] h-[550px] px-4 mx-auto py-1 relative  flex gap-2 justify-around items-center">
             <div className="w-[35%]  h-full py-2 px-2  ">
+              <div className="w-fit h-fit px-2   uppercase   rounded-lg  border flex text-sm font-semibold bg-[#68dd68] text-[#1e451e] leading-6  ">
+                Business
+              </div>
+
               <h1 className="text-[60px] text-[#275527]  font-bold leading-[80px] ">
                 Grow Your Business With Our Help
               </h1>
@@ -38,11 +85,11 @@ const TopHeroSection = () => {
             </div>
           </div>
 
-          <div className=" absolute w-full   h-28  bg-[#576757] bottom-0 -z-1">
-            <div className="w-[50%] h-full border flex  border-red-600  ml-[170px]">
-              <div className="border border-green-500 w-[50%] h-full ">
+          <div className=" absolute w-full  flex justify-between  h-28  bg-[#dbf2db] bottom-0 -z-1">
+            <div className="w-[42%] h-full flex    ml-[180px]">
+              <div className="  w-[50%] h-full ">
                 <div className="flex w-full h-full ">
-                  <div className="img relative w-[50%] h-[134px] top-[-26px] ">
+                  <div className="img relative w-[50%] h-[138px] top-[-26px] ">
                     <Image
                       fill
                       alt="smallimg"
@@ -50,13 +97,64 @@ const TopHeroSection = () => {
                       className="object-contain"
                     />
                   </div>
-                  <div className="txt"></div>
+                  <div className="txt w-[50%] h-full">
+                    <h3 className="font-bold text-md">Who We Are</h3>
+                    <p className="font-semibold text-sm text-gray-600 ">
+                      Learn how to become a sales machine
+                    </p>
+                    <p className="font-semibold text-[12px] cursor-pointer hover:text-blue-500 ">
+                      Learn more
+                    </p>
+                  </div>
                 </div>
               </div>
 
-              <div className="border border-green-500 w-[50%] h-full "></div>
+              <div className=" w-[50%] h-full ">
+                <div className="flex w-full h-full ">
+                  <div className="img relative w-[50%] h-[138px] top-[-26px] ">
+                    <Image
+                      fill
+                      alt="smallimg"
+                      src="https://assets.website-files.com/647650ba58f7bcb914cadd85/64768d42449bbb066bd6d5c0_Hero-guy.png"
+                      className="object-contain"
+                    />
+                  </div>
+
+                  <div className="txt w-[50%] h-full">
+                    <h3 className="font-bold text-md">Small Business</h3>
+
+                    <p className="font-semibold text-sm text-gray-600 ">
+                      See how can we help your business
+                    </p>
+                    <p className="font-semibold text-[12px] cursor-pointer hover:text-blue-500 ">
+                      Learn more
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
+
+            <BsArrowDown
+              size={45}
+              className="  h-20 mr-16 mt-20 bg-[#8ae48a] animate-bounce  rounded-full"
+            />
           </div>
+        </div>
+
+        <Intro />
+
+        <div className="w-fit h-fit    mx-auto  py-2 px-2 grid grid-cols-3  justify-between items-center  gap-3">
+          {aboutBoxData.map((el, index) => {
+            return (
+              <div key={index}>
+                <AboutBox
+                  text={el.text}
+                  pharagraf={el.pharagraf}
+                  icon={el.icon}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
 
