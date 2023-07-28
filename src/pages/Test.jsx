72d7b086edd8de37b55644ee1bf262258df9a790
shivamonside <shivam.dubey@skilzen.com>
2023-07-28 15:00:53 +0530

    added something

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -31,7 +31,7 @@ const Test = () => {
 
         {/* {isActive ? <QuizHome /> : <Instructions setIsActive={setIsActive} />} */}
         {/* <Predictors/> */}
-        {/* <MyCarousel/> */}
+      
 
         {/* <Carousel/> */}
         {/* <Dailyquizhome /> */}
