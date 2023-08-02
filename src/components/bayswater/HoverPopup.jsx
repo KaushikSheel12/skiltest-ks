0248d8184482ef7148adb0d9010b7dfb21df1ff4
shivamonside <shivam.dubey@skilzen.com>
2023-08-02 11:50:43 +0530

    added hover component

--- src/components/bayswater/HoverPopup.jsx
+++ src/components/bayswater/HoverPopup.jsx
@@ -1,79 +1,5 @@
-// import { useState } from "react";
-// import { SiReactquery } from "react-icons/si";
-
-// const HoverBox = () => {
-//   const [isBigBoxOpen, setIsBigBoxOpen] = useState(false);
-//   const [isSmallBoxOpen, setIsSmallBoxOpen] = useState([false, false, false]);
-
-//   const handleBigBoxHover = () => {
-//     setIsBigBoxOpen(true);
-//   };
-
-//   const handleBigBoxLeave = () => {
-//     setIsBigBoxOpen(false);
-//   };
-
-//   const handleSmallBoxHover = (index) => {
-//     const updatedSmallBoxOpen = isSmallBoxOpen.map((_, i) => i === index);
-//     setIsSmallBoxOpen(updatedSmallBoxOpen);
-//   };
-
-//   return (
-//     <div
-//       className="nav border w-full text-lg h-20 px-2 py-2 flex justify-around items-center relative"
-//       onMouseEnter={handleBigBoxHover}
-//       onMouseLeave={handleBigBoxLeave}
-//     >
-//       <h2>hoverme</h2>
-//       <h2>hoverme</h2>
-//       <h2>hoverme</h2>
-//       {isBigBoxOpen && (
-//         <div className="bg-[#c0c0e5] absolute top-20 px-5  w-[90%] left-20  h-[400px]    items-center z-10 ">
-
-// <div className="flex justify-around ">
-//  <div className="topbox  border border-red-600 w-[390px] h-[200px]"></div>
-//  <div className="topbox  border border-red-600 w-[390px] h-[200px]"></div>
-//  <div className="topbox   border border-red-600 w-[390px] h-[200px]"></div>
-
-//  </div>
-
-//    <div className="border border-green-500 flex justify-around  ">
-//           {isSmallBoxOpen.map((isOpen, index) => (
-//             <div
-//               key={index}
-//               className={`bg-teal-300 group  p-4  rounded-md  h-48 ${
-//                 isOpen ? "w-[390px] h-[240px]  mt-[-20px]  " : "w-[390px] h-[100px] "
-//               } transition-all ease-linear`}
-//               onMouseEnter={() => handleSmallBoxHover(index)}
-//               onMouseLeave={() => handleSmallBoxHover(-1)}
-//             >
-
-
-//    <div className="flex gap-5  items-center justify-center">
-// <SiReactquery size={35} color="red" />
-//                 <h4 className="text-2xl font-semibold">Skilzen</h4>
-//                 </div>
-//                 <p className="hidden group-hover:block">We offer a truly unique experience at each of our 10 </p>
-//   </div>
-            
-//           ))}
-
-
-// </div>
-
-
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default HoverBox;
-
-
 import { useState } from "react";
 import { SiReactquery } from "react-icons/si";
-
 
 const HoverBox = () => {
   const [isBigBoxOpen, setIsBigBoxOpen] = useState(false);
@@ -130,6 +56,29 @@ const HoverBox = () => {
               </div>
             ))}
           </div>
+
+
+
+
+          <div className=" flex mt-4 justify-around">
+            {[0, 1, 2].map((index) => (
+              <div
+                key={index}
+                className={`bg-teal-300 group p-4 rounded-md h-48 w-[390px] ${
+                  hoveredSmallBox === index ? "h-[240px] mt-[-100px] border border-yellow-500  " : "h-[100px]"
+                } transition-all ease-linear`}
+                onMouseEnter={() => handleSmallBoxHover(index)}
+                onMouseLeave={() => handleSmallBoxHover(-1)}
+              >
+                <div className="flex gap-5 items-center justify-center">
+                  <SiReactquery size={35} color="red" />
+                  <h4 className="text-2xl font-semibold">Skilzen</h4>
+                </div>
+                <p className="hidden group-hover:block">We offer a truly unique experience at each of our 10 </p>
+              </div>
+            ))}
+          </div>
+           
         </div>
       )}
     </div>
