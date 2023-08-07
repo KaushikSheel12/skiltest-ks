308b5fe879f491f69249e09108a0ad612e910a1a
shivamonside <shivam.dubey@skilzen.com>
2023-08-07 18:02:46 +0530

    added

--- src/components/bayswater/Admitkart/Admitkart.jsx
+++ src/components/bayswater/Admitkart/Admitkart.jsx
@@ -42,8 +42,28 @@ const Admitkart = () => {
             </div>
           </div>
 
-          <div className="w-[35%] h-[600px] rounded-full flex items-center border-[#6216a5e8]  absolute right-[200px]  border py-2 px-2">
-            <div className="w-[80%] h-[480px] px-8 py-8 border border-[#721cbee8] mx-auto rounded-full">
+
+       
+<div className="border  w-[500px] ml-[110px]  grid place-content-center ">
+    <div className="relative w-full h-full z-20">
+    <div className="w-[120px] h-[120px] rounded-full  absolute top-[68px] -left-[104px]">
+        <div className="">
+        <div className="w-full h-full rounded-full relative">
+                    <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfUhb79s2S2_ehx5GoHiMldcdzuGEMDggYCvm2-wzxA2H4q1zYPUFqMLVUYOmHHRtDLkI&usqp=CAU" alt="student" fill className="rounded-full object-cover"/>
+             <p className="w-[193px] left-7 p-1 bg-white border shadow-lg z-30 text-sm  px-4 -bottom-[363px] absolute rounded-lg whitespace-nowrap"><strong className="whitespace-nowrap">Arnav</strong> looking forward to <br /> join Oxfard University</p>
+        </div>
+               
+                </div>
+                <div className="border w-fit h-fit px-5 py-1 bg-white text-black text-sm whitespace-nowrap rounded-lg mt-[-30px] absolute  ml-[-29px] "> <span className="font-semibold">Clark</span>  is an alumni from <br /> Oxford university</div>
+
+            </div>
+    </div>
+<div className="w-[600px] h-[600px] rounded-full flex items-center border-yellow-800  absolute  border py-2 px-2 rotate right-[223px] top-[24px]">
+
+<div className="bg-red-700 rounded-full h-5 w-5 absolute -top-0 right-0 translate-x-[-205px] "/>
+</div>
+            
+            <div className=" px-8 py-8 border border-green-400 mx-auto rounded-full w-[27em] h-[27em]">
             <div className="w-full h-full border mx-auto rounded-full relative">
               <Image
                 src="https://img.freepik.com/free-photo/front-view-female-student-white-shirt-holding-pen-copybook-thinking-blue-wall_140725-38457.jpg"
@@ -52,23 +72,18 @@ const Admitkart = () => {
                 alt="thingking-img"
               />
 
-              <div className="w-11 h-11 right-[385px] rounded-full  absolute top-[240px]">
+              <div className="w-11 h-11 right-[321px] rounded-full   absolute top-[244px]">
                 <div className="w-full h-full rounded-full relative">
                     <Image src="https://m.media-amazon.com/images/I/718gVU8ktKL._AC_UF894,1000_QL80_.jpg" fill alt="indian-flag" className="rounded-full"/>
                 </div>
               </div>
             </div>
             </div>
+</div>
 
 
-            <div className="w-[120px] h-[120px] rounded-full  absolute top-[130px] right-[540px]">
-                <div className="w-full h-full rounded-full relative">
-                    <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfUhb79s2S2_ehx5GoHiMldcdzuGEMDggYCvm2-wzxA2H4q1zYPUFqMLVUYOmHHRtDLkI&usqp=CAU" alt="student" fill className="rounded-full object-cover"/>
-                </div>
-                <div className="border w-fit h-fit px-3 py-1 bg-white text-black text-sm whitespace-nowrap rounded-lg mt-[-30px] absolute   "> <span className="font-semibold">Clark</span>  is an alumni from <br /> Oxford university</div>
-
-            </div>
-          </div>
+          
+  
         </div>
       </div>
     </>
