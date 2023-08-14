01a01789bfc0b637b59185a7d5d3f75c3efaf301
shivamonside <shivam.dubey@skilzen.com>
2023-08-14 22:39:26 +0530

    added

--- src/components/QSidebar/QuestionTabs.tsx
+++ src/components/QSidebar/QuestionTabs.tsx
@@ -22,8 +22,10 @@ export const QuestionTabs = ({ onQuestionClick, isClicked }) => {
 
   return (
     <div>
+
       <div className="grid grid-cols-5 fixed gap-5 py-2 overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-white h-[200px] px-5">
         {questions?.map((el, index) => {
+
           const isQuestionClicked = isClicked(el.id);
           const isCurrentQuestion = questionIds.includes(el.id); // Check if questionIds includes the current question id
 
@@ -32,6 +34,7 @@ export const QuestionTabs = ({ onQuestionClick, isClicked }) => {
             questionIds.map((el) => {
               //  console.log("el", el);
             });
+
           }, [questionIds]);
         
 
@@ -39,6 +42,7 @@ export const QuestionTabs = ({ onQuestionClick, isClicked }) => {
             <div key={index} className="py-0 relative">
               <button
                 onClick={() => {
+                  
                   onQuestionClick(el.id);
                   isClicked(el.id);
                   handleViewed(el)
