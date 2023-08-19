39f91706a2bfbdceca73fc13eac0fe5596472254
shivamonside <shivam.dubey@skilzen.com>
2023-08-19 11:43:11 +0530

    added

--- src/components/abmerblog/Asprogress.jsx
+++ src/components/abmerblog/Asprogress.jsx
@@ -22,10 +22,11 @@ const Asprogress = () => {
         <h2 className="absolute  md:left-[595px] left-[100px] md:top-[312px] font-semibold md:text-[33px] text-md">
           First job
         </h2>
-        <div className="w-[450px] h-[110px] border absolute  left-[795px] top-[287px] flex items-center justify-between text-[23px] text-white px-4 py-4 bg-gray-800">
+        <div className="w-[450px] h-[110px] border absolute  left-[845px] top-[287px] flex items-center justify-between text-[23px] text-white px-4 py-4 bg-gray-800">
           <div>
             <h2>Juior Engineer</h2>
             <h2>28,000</h2>
+            <div style={{clipPath:"polygon(100% 100%, 67.8% 46%, 100% 0%)"}} className="h-[60px] w-[140px] bg-gray-800 absolute right-[448px] top-[25px]"/>
           </div>
           <RiPencilRuler2Line size={65} />
         </div>
@@ -33,7 +34,7 @@ const Asprogress = () => {
         <div className="w-[10px] h-[10px] rounded-full bg-teal-400 left-[733px] absolute top-[335px] "></div>
 
         <div className="w-[300px] h-[170px] border-t-2 border-teal-400 border-r-2 mx-auto absolute border-b-2  left-[737px]">
-          <div className="w-[450px] h-[110px] border absolute right-[100px] top-[30px] flex items-center justify-between text-[23px] text-white px-4 py-4 bg-teal-600">
+          <div className="w-[450px]  h-[110px] border absolute right-[100px] top-[30px] flex items-center justify-between text-[23px] text-white px-4 py-4 bg-teal-600">
             <MdOutlineSettings size={57} color="white" />
             <div>
               <h2> Engineer</h2>
@@ -43,6 +44,7 @@ const Asprogress = () => {
             <h4 className=" absolute  font-semibold left-[600px] text-gray-800 mt-2 whitespace-nowrap">
               + 9 months
             </h4>
+            <div style={{clipPath:"polygon(37.6% 49.3%, 0% 100%, 0% 0%)"}} className="h-[60px] w-20 bg-teal-600 absolute left-[448px] top-[25px]"/>
           </div>
         </div>
 
@@ -51,12 +53,14 @@ const Asprogress = () => {
             <div>
               <h2>Senior Engineer</h2>
               <h2>42,000</h2>
+              <div style={{clipPath:"polygon(100% 100%, 67.8% 46%, 100% 0%)"}} className="h-[60px] w-40 bg-pink-600 absolute right-[448px] top-[25px]"/>
             </div>
             <AiOutlineCodeSandbox size={57} color="white" />
             <div className="w-[10px] h-[10px] absolute  top-12  right-[545px] rounded-full bg-teal-400 mx-auto"></div>
             <h4 className=" absolute  font-semibold right-[600px] text-gray-800 mt-2 whitespace-nowrap">
               + 56 months
             </h4>
+            
           </div>
         </div>
 
@@ -71,17 +75,19 @@ const Asprogress = () => {
             <h4 className=" absolute  font-semibold left-[600px] text-gray-800 mt-2 whitespace-nowrap">
               + 64 months
             </h4>
+            <div style={{clipPath:"polygon(37.6% 49.3%, 0% 100%, 0% 0%)"}} className="h-[60px] w-20 bg-blue-500 absolute left-[448px] top-[25px]"/>
           </div>
           <div className="h-[140px] absolute top-[167px] w-0 mx-auto border border-teal-400 "></div>
           <div className="w-[10px] h-[10px] absolute  top-[307px]  left-[-4px] rounded-full bg-teal-400 mx-auto"></div>
         </div>
 
-        <div className="w-[450px] h-[160px] border absolute left-[800px] top-[1000px]  text-left text-[23px] text-white px-4 py-4 bg-yellow-500">
+        <div className="w-[450px] h-[160px] border absolute left-[820px] top-[1000px]  text-left text-[23px] text-white px-4 py-4 bg-yellow-500">
           <BsStar size={57} color="white" />
 
           <div className="mt-4 ml-2">
             <h2>Director of Engineering</h2>
             <h2>100K +</h2>
+            <div style={{clipPath:"polygon(100% 100%, 67.8% 46%, 100% 0%)"}} className="h-[60px] w-40 bg-yellow-500 absolute right-[448px] top-[50px]"/>
           </div>
 
           <h4 className=" absolute  font-semibold right-[600px] top-[0px] text-gray-800 mt-2 whitespace-nowrap">
