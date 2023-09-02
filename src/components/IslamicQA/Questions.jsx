f3032d4f397fceccba9c3c4b0aae1c65f631917a
shivamonside <shivam.dubey@skilzen.com>
2023-09-03 01:26:18 +0530

    added Islamink question component

--- src/components/IslamicQA/Questions.jsx
+++ src/components/IslamicQA/Questions.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { CiSaveDown2 } from "react-icons/ci";
+import QBox from "./QBox";
 
 const Questions = () => {
   return (
-  <>
-  <div className='w-[80%] mx-auto h-[500px] border px-2 py-2 '></div>
-  </>
-  )
-}
+    <>
+      <div className="md:w-[70%] w-full mx-auto h-full border px-4 md:px-2 py-2 ">
+        <div className="text-center">
+          <h1 className="font-bold text-[25px] md:text-[35px]">New Answers of Question </h1>
+        </div>
 
-export default Questions
+        <div className="grid md:grid-cols-3 grid-cols-1 mt-5 md:mt-10">
+          {[...new Array(15)].map((el, index) => {
+            return (
+              <div key={index}>
+                <QBox
+                  Ques="Q: She is asking about halaal & haraam with regard to freelancework "
+                  date="18 September 2021"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Questions;
