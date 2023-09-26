25ec0badf155e7b61bfb113f415421ab90a85929
shivamonside <shivam.dubey@skilzen.com>
2023-09-26 22:40:43 +0530

    Added

--- src/components/predictors/NewComp.jsx
+++ src/components/predictors/NewComp.jsx
@@ -11,26 +11,27 @@ export const NewPredictorBox = () => {
             alt="text"
             width={40}
             height={40}
-            
           />
           <h1 className="text-[17px] font-semibold text-gray-700">
-          JEE(Main) 2023
+            JEE(Main) 2023
           </h1>
         </div>
         <div className="mt-3">
-        
           <p className="text-[14px]  leading-4 text-center text-gray-500">
             {" "}
-         predict chances in  <span className="text-blue-400">Aditya College Of Nursing,</span> Hyderabad
+            predict chances in{" "}
+            <span className="text-blue-400">
+              Aditya College Of Nursing,
+            </span>{" "}
+            Hyderabad
           </p>
-     
         </div>
 
         <div className="flex items-center mt-3 justify-between ">
           <div className="flex items-center gap-4   w-full text-gray-500 text-md">
-            <p className="border-r-2 px-2 ">12/9/2023</p>
-            <p className="border-r-2 px-2 ">text 2</p>
-            <p className=" ">text 3</p>
+            <p className="border-r-2  pr-2">12/9/2023</p>
+            <p className="border-r-2  pr-2">Exam info</p>
+            <p className=" mr-2">practice test</p>
           </div>
 
           <button className="text-sm py-[2px] w-fit px-3 rounded-md border bg-blue-500 text-white font-semibold ">
