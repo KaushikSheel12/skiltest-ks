7b7f5fdbbb847eef2bce1900fcb92c8fdce35343
shivamonside <shivam.dubey@skilzen.com>
2023-10-24 01:06:32 +0530

    added resorses

--- src/components/kreativetools/ResourceNav.jsx
+++ src/components/kreativetools/ResourceNav.jsx
@@ -1,29 +1,28 @@
-import useToggleStore from '@/zustand';
-import React from 'react'
-import { BsDoorClosed, BsMenuDown } from 'react-icons/Bs';
+import useToggleStore from "@/zustand";
+import React from "react";
+import { RxCross2 } from "react-icons/Rx";
+import { AiOutlineUnorderedList } from "react-icons/ai";
 
 export const ResourceNev = () => {
-    const { isToggel, setIsToggel } = useToggleStore();
+  const { isToggel, setIsToggel } = useToggleStore();
   return (
-  <>
-    <ToggelSidebarButton setIsToggel={setIsToggel} isToggel={isToggel} />
-  </>
-  )
-}
-
+    <>
+      <ToggelSidebarButton setIsToggel={setIsToggel} isToggel={isToggel} />
+    </>
+  );
+};
 
 const ToggelSidebarButton = ({ setIsToggel, isToggel }) => {
-    return (
-      <div
-        className="p-2 rounded-md bg-black  fixed md:hidden block z-20 top-2 right-6"
-        onClick={() => setIsToggel(!isToggel)}
-      >
-        {isToggel ? (
-          <BsDoorClosed size={16} color="#fff" />
-        ) : (
-          <BsMenuDown size={16} color="#fff" />
-        )}
-      </div>
-    );
-  };
-  
+  return (
+    <div
+      className="p-2 rounded-md bg-black  fixed md:hidden block z-20 top-2 right-6"
+      onClick={() => setIsToggel(!isToggel)}
+    >
+      {isToggel ? (
+        <RxCross2 size={16} color="#fff" />
+      ) : (
+        <AiOutlineUnorderedList size={16} color="#fff" />
+      )}
+    </div>
+  );
+};
