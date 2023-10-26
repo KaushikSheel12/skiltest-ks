60c1d5891882ab05e50a771026fe98242c2bb2ad
shivamonside <shivam.dubey@skilzen.com>
2023-10-27 01:19:01 +0530

    added home-page

--- src/components/Relieve/RelieveLandingPage.jsx
+++ src/components/Relieve/RelieveLandingPage.jsx
@@ -50,7 +50,7 @@ export const RelieveLandingPage = () => {
           </p>
 
           <div className="flex  gap-16 items-center w-full h-fit">
-            <div className="w-fit cursor-pointer h-fit py-2 px-6 border rounded-md mt-8 bg-green-400 hover:bg-green-700 flex items-center gap-2">
+            <div className="w-fit cursor-pointer h-fit py-2 px-6  rounded-md mt-8 bg-green-400 hover:bg-green-700 flex items-center gap-2">
               <p className="text-[14px]">Get started now </p>{" "}
               <BsArrowRight color="black" />
             </div>
@@ -86,7 +86,125 @@ export const RelieveLandingPage = () => {
             );
           })}
         </div>
-        <div className="h-[1000px] w-2"></div>
+        <div className="w-full mt-20 h-[200px] px-20  grid place-content-center  bg-[#E7F2F0]">
+          <div className="flex items-center   gap-x-52  justify-between">
+            <div>
+              <h2 className="text-[30px]  text-green-900">
+                Have items you want to get rid of?
+              </h2>
+              <p className="text-[18px] text-[#8daa8d]">
+                Recycle them through Relieve and see how easy and quick it is to
+                turn unwanted items into positive climate impact.
+              </p>
+            </div>
+            <div className="w-fit h-fit px-4 py-2 cursor-pointer bg-green-900 text-white flex items-center gap-2  rounded-md ">
+              <p>Start now</p> <BsArrowRight color="white" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full  mt-20 h-screen relative">
+        <Image
+          fill
+          src="https://www.relievefurniture.com/_next/image?url=%2Fstatic%2Fimages%2Fv3%2Foffice-furniture-in-land-fill.jpg&w=3840&q=75"
+          alt="bg-img"
+          loading="lazy"
+          className="object-cover"
+        />
+        <div className=" absolute top-28  left-[130px] w-[85%] h-fit">
+          <p className="text-[16px] text-white ">OUR MISSION</p>
+          <h1 className="text-[60px]  text-white">
+            Every year,
+            <span className="border-b-2">
+              45 million items of furniture are sent to landfill in Europe
+              alone.
+            </span>
+            Does this bother you, too? Relieve supports organizations to
+            connect, buy, and recycle lightly used office furniture and items,
+            <span className="border-b-2">
+              reducing costs to the environment and your business.
+            </span>
+          </h1>
+          <div className="flex mt-20 items-center justify-between w-full h-fit">
+            <p className="text-[20px] text-white">
+              Does this mission speak to you? Or have project in mind
+            </p>
+
+            <div className="w-fit cursor-pointer h-fit py-2 px-6  rounded-md mt-8 bg-green-400 hover:bg-green-700 flex items-center gap-2">
+              <p className="text-[14px]">Plan a call </p>
+              <BsArrowRight color="black" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-[85%] mx-auto mt-20">
+        <div className="w-full  h-[200px] px-20  grid place-content-center  bg-[#E7F2F0]">
+          <div className="flex items-center   gap-x-52  justify-between">
+            <div>
+              <h2 className="text-[30px]  text-green-900">
+                Circularity matters
+              </h2>
+              <p className="text-[18px] text-[#8daa8d]">
+                Creating more circular economies are essential to the future of
+                business, and can be the key to drastically lowering corporate
+                carbon emissions… as well as costs
+              </p>
+            </div>
+            <div className="w-[200px] h-fit px-2 py-2 cursor-pointer bg-green-900 text-white flex items-center justify-around rounded-md ">
+              <p>Read more</p> <BsArrowRight color="white" />
+            </div>
+          </div>
+        </div>
+
+        <h1 className="text-[50px] mt-20 text-green-900">
+          Furnishing Solutions
+        </h1>
+        <div className=" grid grid-cols-4 mt-3 gap-3">
+          {PlateformImages?.map((el, index) => {
+            return (
+              <div className="w-[350px] h-fit">
+                <div className="w-full h-[435px]  rounded-md relative ">
+                  <Image
+                    src="https://www.relievefurniture.com/_next/image?url=%2Fstatic%2Fimages%2Fv3%2Fconsultations.jpg&w=828&q=75"
+                    alt="img"
+                    fill
+                    className="object-cover rounded-md"
+                    loading="lazy"
+                  />
+                </div>
+                <div className="w-full h-[150px] ">
+                  <div className="w-[140px] mt-5 h-[1px] bg-black"></div>
+                  <h2 className="ml-16">1.</h2>
+                  <h1 className="text-[30px] text-green-900">Consultations</h1>
+                  <p className="text-[18px] text-[#777474]">
+                    Get help from a qualified member of the Relieve team to
+                    design and plan any office space with items we have in stock
+                    and arriving soon.
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="w-full mt-20 h-[200px] px-20 border border-gray-600  grid place-content-center  ">
+          <div className="flex items-center   gap-x-52  justify-between">
+            <div>
+              <h2 className="text-[30px]  text-green-900">
+                Circularity matters
+              </h2>
+              <p className="text-[18px] text-[#8daa8d]">
+                Creating more circular economies are essential to the future of
+                business, and can be the key to drastically lowering corporate
+                carbon emissions… as well as costs
+              </p>
+            </div>
+            <div className="w-[200px] h-fit px-2 py-2 cursor-pointer bg-green-900 text-white flex items-center justify-around rounded-md ">
+              <p>Read more</p> <BsArrowRight color="white" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
