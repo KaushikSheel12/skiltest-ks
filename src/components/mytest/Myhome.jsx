ea6cd221a84c2b51bec0328b9fc3adf28fc10a17
shivamonside <shivam.dubey@skilzen.com>
2023-11-16 23:21:36 +0530

    added

--- src/components/mytest/Myhome.jsx
+++ src/components/mytest/Myhome.jsx
@@ -47,12 +47,11 @@ export const Myhome = () => {
         })}
       </div>
 
-
-
       <h2 className="mt-4 md:text-[30px] md:px-0 px-2 text-[15px] font-semibold text-gray-700">
        Contect us 
       </h2>
       <div className="w-full md:h-[4px] h-[2px]  bg-gray-400"></div>
+
 
       <div className="md:w-[500px] px-4 py-4 mx-auto h-fit rounded-md border mt-10">
         <input className="w-full mt-3 border px-2" placeholder="Your full name"/>
@@ -62,6 +61,7 @@ export const Myhome = () => {
         <input className="w-full mt-3 border px-2" placeholder="Your state"/>
         
         <button className=" w-full mt-4 bg-green-500 px-4 py-2  border hover:text-white hover:bg-gray-500 rounded-md">Submit</button>
+        
       </div>
     </div>
   );
